5d29ea7ee8e27bf9ae62d703457e5a067a261ec3
yeyoung8805 <yyk8805@naver.com>
2022-04-03 20:01:57 +0900

    show animalItems in firstPage

--- lib/sub/firstPage.dart
+++ lib/sub/firstPage.dart
@@ -7,13 +7,28 @@ class FirstApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container(
+    return Scaffold(
+        body: Container(
       child: Center(
-        child: ListView.builder(itemBuilder: (context, position) {
-          return Card(//리스트뷰의 아이템은 Card로 만든다.
-
+        child: ListView.builder(
+            itemBuilder: (context, position) {
+              //position 은 아이템의 위치(인덱스)를 의미
+              return Card(
+                //리스트뷰의 아이템은 Card로 만든다.
+                child: Row(
+                  children: <Widget>[
+                    Image.asset(
+                      list![position].imagePath!,
+                      height: 100,
+                      width: 100,
+                      fit: BoxFit.contain,
+                    ),
+                    Text(list![position].animalName!)
+                  ],
+                ),
               );
-        }),
+            },
+            itemCount: list!.length), //아이템 개수만큼만 스크롤할 수 있게 제한
       ),
     ));
   }
