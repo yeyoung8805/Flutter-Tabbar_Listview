6d2ac8c45657a0b916300125f3166d21583e5502
yeyoung8805 <yyk8805@naver.com>
2022-04-03 20:52:51 +0900

    alert when touch items

--- lib/sub/firstPage.dart
+++ lib/sub/firstPage.dart
@@ -13,19 +13,32 @@ class FirstApp extends StatelessWidget {
         child: ListView.builder(
             itemBuilder: (context, position) {
               //position 은 아이템의 위치(인덱스)를 의미
-              return Card(
-                //리스트뷰의 아이템은 Card로 만든다.
-                child: Row(
-                  children: <Widget>[
-                    Image.asset(
-                      list![position].imagePath!,
-                      height: 100,
-                      width: 100,
-                      fit: BoxFit.contain,
-                    ),
-                    Text(list![position].animalName!)
-                  ],
+              return GestureDetector(
+                child: Card(
+                  //리스트뷰의 아이템은 Card로 만든다.
+                  child: Row(
+                    children: <Widget>[
+                      Image.asset(
+                        list![position].imagePath!,
+                        height: 100,
+                        width: 100,
+                        fit: BoxFit.contain,
+                      ),
+                      Text(list![position].animalName!)
+                    ],
+                  ),
                 ),
+                onTap: () {
+                  AlertDialog dialog = AlertDialog(
+                    content: Text(
+                      '이 동물은 ${list![position].kind} 입니다',
+                      style: TextStyle(fontSize: 30.0),
+                    ),
+                  );
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) => dialog);
+                },
               );
             },
             itemCount: list!.length), //아이템 개수만큼만 스크롤할 수 있게 제한
