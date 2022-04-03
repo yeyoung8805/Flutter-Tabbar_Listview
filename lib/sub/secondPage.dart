b7104f5b16f2b373dd3e201cf40e2b2f40b73999
yeyoung8805 <yyk8805@naver.com>
2022-04-03 20:08:42 +0900

    change secondApp to add animal

--- lib/sub/secondPage.dart
+++ lib/sub/secondPage.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import '../animalItem.dart';
 
-class SecondApp extends StatelessWidget {
+class SecondApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _SecondApp();
   final List<Animal>? list;
   SecondApp({Key? key, this.list})
       : super(key: key); //list를 매개변수로 입력받는 생성자가 추가됨
+}
 
+class _SecondApp extends State<SecondApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
       child: Center(
-        child: Text('두번째 페이지'),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[]),
       ),
     ));
   }
