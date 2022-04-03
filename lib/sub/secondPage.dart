505afe3a43710c07b5558ec65560ddd296c341da
yeyoung8805 <yyk8805@naver.com>
2022-04-03 20:16:13 +0900

    add radio button

--- lib/sub/secondPage.dart
+++ lib/sub/secondPage.dart
@@ -10,14 +10,39 @@ class SecondApp extends StatefulWidget {
 }
 
 class _SecondApp extends State<SecondApp> {
+  final nameController = TextEditingController();
+  int? _radioValue = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
       child: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[]),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+            Widget>[
+          TextField(
+            controller: nameController,
+            keyboardType: TextInputType.text,
+            maxLines: 1,
+          ),
+          Row(
+            children: <Widget>[
+              Radio(value: 0, groupValue: _radioValue, onChanged: _radioChange),
+              Text('양서류'),
+              Radio(value: 1, groupValue: _radioValue, onChanged: _radioChange),
+              Text('파충류'),
+              Radio(value: 2, groupValue: _radioValue, onChanged: _radioChange),
+              Text('포유류'),
+            ],
+          )
+        ]),
       ),
     ));
+  }
+
+  _radioChange(int? value) {
+    setState(() {
+      _radioValue = value;
+    });
   }
 }
