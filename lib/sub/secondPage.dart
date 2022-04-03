812913dcc88d68735fe6815397a78ec78f64e6bc
yeyoung8805 <yyk8805@naver.com>
2022-04-03 20:31:18 +0900

    add animals

--- lib/sub/secondPage.dart
+++ lib/sub/secondPage.dart
@@ -13,6 +13,7 @@ class _SecondApp extends State<SecondApp> {
   final nameController = TextEditingController();
   int? _radioValue = 0;
   bool? flyExist = false;
+  String? _imagePath;
 
   @override
   Widget build(BuildContext context) {
@@ -26,28 +27,75 @@ class _SecondApp extends State<SecondApp> {
             keyboardType: TextInputType.text,
             maxLines: 1,
           ),
-          Row(
-            children: <Widget>[
-              Radio(value: 0, groupValue: _radioValue, onChanged: _radioChange),
-              Text('양서류'),
-              Radio(value: 1, groupValue: _radioValue, onChanged: _radioChange),
-              Text('파충류'),
-              Radio(value: 2, groupValue: _radioValue, onChanged: _radioChange),
-              Text('포유류'),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Text('날 수 있나요?'),
-              Checkbox(
-                  value: flyExist,
-                  onChanged: (bool? check) {
-                    setState(() {
-                      flyExist = check;
-                    });
-                  })
-            ],
-          ),
+          Row(children: <Widget>[
+            Radio(value: 0, groupValue: _radioValue, onChanged: _radioChange),
+            Text('양서류'),
+            Radio(value: 1, groupValue: _radioValue, onChanged: _radioChange),
+            Text('파충류'),
+            Radio(value: 2, groupValue: _radioValue, onChanged: _radioChange),
+            Text('포유류'),
+          ], mainAxisAlignment: MainAxisAlignment.spaceAround),
+          Row(children: <Widget>[
+            Text('날 수 있나요?'),
+            Checkbox(
+                value: flyExist,
+                onChanged: (bool? check) {
+                  setState(() {
+                    flyExist = check;
+                  });
+                })
+          ], mainAxisAlignment: MainAxisAlignment.spaceAround),
+          Row(children: <Widget>[
+            GestureDetector(
+              child: Image.asset('repo/images/cow.png', width: 80),
+              onTap: () {
+                _imagePath = 'repo/images/cow.png';
+              },
+            ),
+            GestureDetector(
+              child: Image.asset('repo/images/pig.png', width: 80),
+              onTap: () {
+                _imagePath = 'repo/images/pig.png';
+              },
+            ),
+            GestureDetector(
+              child: Image.asset('repo/images/bee.png', width: 80),
+              onTap: () {
+                _imagePath = 'repo/images/bee.png';
+              },
+            ),
+            GestureDetector(
+              child: Image.asset('repo/images/cat.png', width: 80),
+              onTap: () {
+                _imagePath = 'repo/images/cat.png';
+              },
+            ),
+            GestureDetector(
+              child: Image.asset('repo/images/dog.png', width: 80),
+              onTap: () {
+                _imagePath = 'repo/images/dog.png';
+              },
+            ),
+            GestureDetector(
+              child: Image.asset('repo/images/fox.png', width: 80),
+              onTap: () {
+                _imagePath = 'repo/images/fox.png';
+              },
+            ),
+            GestureDetector(
+              child: Image.asset('repo/images/monkey.png', width: 80),
+              onTap: () {
+                _imagePath = 'repo/images/monkey.png';
+              },
+            ),
+            GestureDetector(
+              child: Image.asset('repo/images/wolf.png', width: 80),
+              onTap: () {
+                _imagePath = 'repo/images/wolf.png';
+              },
+            ),
+          ], mainAxisAlignment: MainAxisAlignment.spaceAround),
+          ElevatedButton(child: Text('동물 추가하기'), onPressed: () {})
         ]),
       ),
     ));
